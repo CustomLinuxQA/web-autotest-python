04a93445164b264e17e9515ac55d2ac7c8ac7dd3
goat <customlinuxqa@gmail.com>
2022-12-11 14:07:34 +0300

    add test methods and files

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,6 @@
 from selenium.common.exceptions import NoSuchElementException
 class BasePage():
-    def __init__(self, browser, url, timeout=10):
+    def __init__(self, browser, url, timeout=5):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
@@ -8,7 +8,7 @@ class BasePage():
     def open(self):
         self.browser.get(self.url)
 
-    def is_element_present(self, how, what):
+    def is_element_present(self, how, what):    # how (CSS/ID/XPATH) what (Selector)
         try:
             self.browser.find_element(how, what)
         except (NoSuchElementException):
---
+++ pages/login_page.py
@@ -0,0 +1,24 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.go_to_login_page()
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def go_to_login_page(self):
+        self.browser.find_element(*LoginPageLocators.LOGIN_LINK).click()
+
+    def should_be_login_url(self):
+        self.is_element_present(*LoginPageLocators.LOGIN_LINK), "Login link is not presented"
+        current_url = self.browser.current_url
+        assert "/accounts/login/" in current_url, "ERROR! It's not login link!"
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
