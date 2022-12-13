9856c3336102be220d93ad829be816b02a9bf8d9
goat <customlinuxqa@gmail.com>
2022-12-13 15:16:26 +0300

    Добавил маркировку в теста @pytest.mark.parametrize для прохождения теста с несколькими продуктами.

--- test/test_product_page.py
+++ test/test_product_page.py
@@ -1,36 +1,40 @@
+import pytest
+
 from pages.product_page import ProductPage
-from pages.url import ProductPageUrl
+from pages.test_link import ProductTestLink
 
 
-def test_guest_can_open_product_page(browser):
-    link = (ProductPageUrl.THE_SHELLCODERS_HANDBOOK_LINK)
-    page = ProductPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url
+@pytest.mark.parametrize("test_link", ProductTestLink.TEST_LINK)
+def test_guest_can_open_product_page(browser, test_link):
+    page = ProductPage(browser,
+                       test_link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url
     page.open()  # открываем страницу
     page.should_be_product_url()  # проверка url
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = (ProductPageUrl.THE_SHELLCODERS_HANDBOOK_LINK)
-    page = ProductPage(browser, link)
+@pytest.mark.parametrize("test_link", ProductTestLink.TEST_LINK)
+def test_guest_can_add_product_to_basket(browser, test_link):
+    page = ProductPage(browser, test_link)
     page.open()
     page.add_item_to_basket()  # Проверка добавления товара в корзину.
 
 
-def test_guest_can_see_added_item_in_notification(browser):
-    link = (ProductPageUrl.THE_SHELLCODERS_HANDBOOK_LINK)
-    page = ProductPage(browser, link)
+@pytest.mark.parametrize("test_link", ProductTestLink.TEST_LINK)
+def test_guest_can_see_added_item_in_notification(browser, test_link):
+    page = ProductPage(browser, test_link)
     page.open()
     page.excepted_item_name_in_notification()  # Проверка соответствия названия товара в уведомлении.
 
 
-def test_guest_can_see_total_price_in_notification(browser):
-    link = (ProductPageUrl.THE_SHELLCODERS_HANDBOOK_LINK)
-    page = ProductPage(browser, link)
+@pytest.mark.parametrize("test_link", ProductTestLink.TEST_LINK)
+def test_guest_can_see_total_price_in_notification(browser, test_link):
+    page = ProductPage(browser, test_link)
     page.open()
-    page.should_be_total_price_notification()  # Проверка соответствия цены товара в уведомлении.
+    page.should_be_correct_total_price_notification()  # Проверка соответствия цены товара в уведомлении.
 
-def test_guest_can_see_total_price_of_items(browser):
-    link = (ProductPageUrl.THE_SHELLCODERS_HANDBOOK_LINK)
-    page = ProductPage(browser, link)
+
+@pytest.mark.parametrize("test_link", ProductTestLink.TEST_LINK)
+def test_guest_can_see_total_price_of_items(browser, test_link):
+    page = ProductPage(browser, test_link)
     page.open()
     page.excepted_total_price_of_items()  # Стоимость корзины совпадает с ценой товара.
