25d405f644fb5d0533f57f41ec8adcc0c10e58ff
goat <customlinuxqa@gmail.com>
2022-12-13 15:14:35 +0300

    Переписал url файл, создав 2 сущности: BASE_URL и PROMO_URL.

--- pages/url.py
+++
@@ -1,8 +0,0 @@
-class ProductPageUrl():
-    BASE_URL = "http://selenium1py.pythonanywhere.com/"
-
-    THE_SHELLCODERS_HANDBOOK_ENDPOINT ="catalogue/the-shellcoders-handbook_209/"
-    THE_SHELLCODERS_HANDBOOK_LINK = BASE_URL + THE_SHELLCODERS_HANDBOOK_ENDPOINT
-
-    THE_SHELLCODERS_HANDBOOK_PROMO_ENDPOINT = "catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    THE_SHELLCODERS_HANDBOOK_PROMO_LINK = BASE_URL + THE_SHELLCODERS_HANDBOOK_PROMO_ENDPOINT
