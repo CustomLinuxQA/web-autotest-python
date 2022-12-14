58d98fa11f4bdbfed59a0560cb89963b15c6ffb6
goat <customlinuxqa@gmail.com>
2022-12-14 12:40:47 +0300

    Добавил локаторы

--- pages/locators.py
+++ pages/locators.py
@@ -4,16 +4,32 @@ from selenium.webdriver.common.by import By
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_LOCATOR = (By.CSS_SELECTOR, "a[class$='btn-default']")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
+
+
+class BasketPageLocators():
+    CHECKOUT_LOCATOR = (By.CSS_SELECTOR, "a[class$='btn-block']")
+    BASKET_IS_EMPTY_LOCATOR = (By.CSS_SELECTOR, "div p")
 
 
 class LoginPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    DELETE_PASS_INPUT = (By.CSS_SELECTOR, "div [name='password']")
+    DELETE_PROFILE_BUTTON = (By.CSS_SELECTOR, "#delete_profile")
+    DELETE_PROFILE_BUTTON_CONFIRM = (By.CSS_SELECTOR, ".btn-danger")
+    LOGIN_BUTTON = (By.CSS_SELECTOR, "button[value='Log In']")
     LOGIN_FORM = (By.ID, "login_form")
+    LOGIN_EMAIL_INPUT = (By.CSS_SELECTOR, "#login_form [name='login-username']")
+    LOGIN_PASS_INPUT = (By.CSS_SELECTOR, "#login_form [name='login-password']")
+    REGISTER_BUTTON =(By.CSS_SELECTOR, "button[value=Register]")
     REGISTER_FORM = (By.ID, "register_form")
+    REGISTER_EMAIL_INPUT = (By.CSS_SELECTOR, "#register_form [name='registration-email']")
+    REGISTER_PASS_INPUT = (By.CSS_SELECTOR, "#register_form [name='registration-password1']")
+    REGISTER_PASS_CONFIRM_INPUT = (By.CSS_SELECTOR, "#register_form [name='registration-password2']")
 
 
 class MainPageLocators():
-    TEST_LOCATOR = (By.CSS_SELECTOR, "#test")
+    MOK_LOCATOR = (By.CSS_SELECTOR, "#mok")
 
 
 class ProductPageLocators():
