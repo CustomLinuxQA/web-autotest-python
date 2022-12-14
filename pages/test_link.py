e2d9a1db709d78e331171dfd05352ee082539a38
goat <customlinuxqa@gmail.com>
2022-12-14 12:40:26 +0300

    Рефакторинг линков

--- pages/test_link.py
+++ pages/test_link.py
@@ -1,4 +1,12 @@
 from pages.url_rout import ProductPageUrl
+from pages.url_rout import ProductBugPageUrl    # BUG TUSK lesson3_step4.
+from pages.url_rout import MainPageUrl
+
+
+class MainTestLink():
+    TEST_LINK = [
+        (MainPageUrl.MAIN_PAGE_LINK)
+    ]
 
 
 class ProductTestLink():
@@ -6,7 +14,20 @@ class ProductTestLink():
         (ProductPageUrl.THE_SHELLCODERS_HANDBOOK_LINK),
         (ProductPageUrl.THE_SHELLCODERS_HANDBOOK_PROMO_LINK),
         (ProductPageUrl.CODERS_AT_WORK_LINK),
-        (ProductPageUrl.CODERS_AT_WORK_PROMO_LINK),
-        # (ProductPageUrl.CODERS_AT_WORK_BUG_LINK),
-        # (ProductPageUrl.CODERS_AT_WORK_BUG_PROMO_LINK)    # для тестового задания.
+        (ProductPageUrl.CODERS_AT_WORK_PROMO_LINK)
+    ]
+
+
+class ProductBugTestLink():    # BUG TUSK lesson3_step4.
+    TEST_LINK = [
+        (ProductBugPageUrl.CODERS_AT_WORK_BUG_PROMO_LINK) + "0",
+        (ProductBugPageUrl.CODERS_AT_WORK_BUG_PROMO_LINK) + "1",
+        (ProductBugPageUrl.CODERS_AT_WORK_BUG_PROMO_LINK) + "2",
+        (ProductBugPageUrl.CODERS_AT_WORK_BUG_PROMO_LINK) + "3",
+        (ProductBugPageUrl.CODERS_AT_WORK_BUG_PROMO_LINK) + "4",
+        (ProductBugPageUrl.CODERS_AT_WORK_BUG_PROMO_LINK) + "5",
+        (ProductBugPageUrl.CODERS_AT_WORK_BUG_PROMO_LINK) + "6",
+        (ProductBugPageUrl.CODERS_AT_WORK_BUG_PROMO_LINK) + "7",
+        (ProductBugPageUrl.CODERS_AT_WORK_BUG_PROMO_LINK) + "8",
+        (ProductBugPageUrl.CODERS_AT_WORK_BUG_PROMO_LINK) + "9"
     ]
--- pages/url_rout.py
+++ pages/url_rout.py
@@ -1,14 +1,11 @@
+class MainPageUrl():
+    BASE_URL = "http://selenium1py.pythonanywhere.com/"
+
+    MAIN_PAGE_LINK = BASE_URL
+
 class ProductPageUrl():
     BASE_URL = "http://selenium1py.pythonanywhere.com/"
     PROMO_URL = "?promo=newYear2019"
-
-    # BUG FUNC
-    PROMO_BUG_URL = "?promo=offer7"
-
-    # BUG TUSK
-    CODERS_AT_WORK_BUG_ENDPOINT = "catalogue/coders-at-work_207/"
-    CODERS_AT_WORK_BUG_LINK = BASE_URL + CODERS_AT_WORK_BUG_ENDPOINT
-    CODERS_AT_WORK_BUG_PROMO_LINK = BASE_URL + CODERS_AT_WORK_BUG_ENDPOINT + PROMO_BUG_URL
 
     THE_SHELLCODERS_HANDBOOK_ENDPOINT = "catalogue/the-shellcoders-handbook_209/"
     THE_SHELLCODERS_HANDBOOK_LINK = BASE_URL + THE_SHELLCODERS_HANDBOOK_ENDPOINT
@@ -21,3 +18,11 @@ class ProductPageUrl():
     THE_CITY_AND_THE_STARS_95_ENDPOINT = "catalogue/the-city-and-the-stars_95/"
     THE_CITY_AND_THE_STARS_95_LINK = BASE_URL + THE_CITY_AND_THE_STARS_95_ENDPOINT
     THE_CITY_AND_THE_STARS_95_PROMO_LINK = BASE_URL + THE_CITY_AND_THE_STARS_95_ENDPOINT + PROMO_URL
+
+class ProductBugPageUrl():
+    BASE_URL = "http://selenium1py.pythonanywhere.com/"
+    PROMO_URL = "?promo=offer"
+
+    # BUG TUSK lesson3_step4.
+    CODERS_AT_WORK_BUG_ENDPOINT = "catalogue/coders-at-work_207/"
+    CODERS_AT_WORK_BUG_PROMO_LINK = BASE_URL + CODERS_AT_WORK_BUG_ENDPOINT + PROMO_URL
