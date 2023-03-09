b8e0c6e71b98eddde162743beb6bc5176f3438c4
goat <customlinuxqa@gmail.com>
2023-03-09 17:50:06 +0300

    Добавил allure screenshot.

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,5 @@
+import allure
+from allure_commons.types import AttachmentType
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.wait import WebDriverWait
@@ -13,10 +15,15 @@ class BasePage():
 
     def open(self):
         self.browser.get(self.url)
+        with allure.step('Method Open'):
+            allure.attach(self.browser.get_screenshot_as_png(), name='Screenshot', attachment_type=AttachmentType.PNG)
 
     def is_element_present(self, how, what):  # how (CSS/ID/XPATH) what (Selector)
         try:
             self.browser.find_element(how, what)
+            with allure.step('Method is_element_present'):
+                allure.attach(self.browser.get_screenshot_as_png(), name='Screenshot',
+                              attachment_type=AttachmentType.PNG)
         except (NoSuchElementException):
             return False
         return True
@@ -24,6 +31,9 @@ class BasePage():
     def is_not_element_present(self, how, what, timeout=4):
         try:
             WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+            with allure.step('Method is_not_element_present'):
+                allure.attach(self.browser.get_screenshot_as_png(), name='Screenshot',
+                              attachment_type=AttachmentType.PNG)
         except TimeoutException:
             return True
 
@@ -33,6 +43,9 @@ class BasePage():
         try:
             WebDriverWait(self.browser, timeout, 1, TimeoutException). \
                 until_not(EC.presence_of_element_located((how, what)))
+            with allure.step('Method is_disappeared'):
+                allure.attach(self.browser.get_screenshot_as_png(), name='Screenshot',
+                              attachment_type=AttachmentType.PNG)
         except TimeoutException:
             return False
 
@@ -40,12 +53,20 @@ class BasePage():
 
     def go_to_login_page(self):
         self.browser.find_element(*BasePageLocators.LOGIN_LINK).click()
+        with allure.step('Method go_to_login_page'):
+            allure.attach(self.browser.get_screenshot_as_png(), name='Screenshot', attachment_type=AttachmentType.PNG)
 
     def go_to_account_page(self):
         self.browser.find_element(*BasePageLocators.USER_ICON).click()
+        with allure.step('Method go_to_account_page'):
+            allure.attach(self.browser.get_screenshot_as_png(), name='Screenshot', attachment_type=AttachmentType.PNG)
 
     def go_to_basket(self):
         self.browser.find_element(*BasePageLocators.BASKET_LOCATOR).click()
+        with allure.step('Method go_to_basket'):
+            allure.attach(self.browser.get_screenshot_as_png(), name='Screenshot', attachment_type=AttachmentType.PNG)
 
     def should_be_login_link(self):
         assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "AT ERROR! Login link is not presented"
+        with allure.step('Method should_be_login_link'):
+            allure.attach(self.browser.get_screenshot_as_png(), name='Screenshot', attachment_type=AttachmentType.PNG)
